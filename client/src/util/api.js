dd17fa577e77d1620385d9b22b2d110c4ff2e026
Alex <alex.kliger@gmail.com>
2022-09-25 11:08:41 -0400

    added api functions that request to increment the current question of game

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -14,3 +14,13 @@ export const startGame = async (gameId) => {
     const config = {body: JSON.stringify({gameId: gameId})}
     return await POST('http://localhost:5000/game/start', config)
 }
+
+export const nextQuestion = async (gameId) => {
+    const config = {body: JSON.stringify({ gameId: gameId })}
+    return await POST('http://localhost:5000/game/questions/next', config)
+}
+
+export const prevQuestion = async (gameId) => {
+    const config = {body: JSON.stringify({ gameId: gameId })}
+    return await POST('http://localhost:5000/game/questions/previous', config)
+}
