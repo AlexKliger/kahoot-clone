40b1844f34eeeabcca56518e329e8230c493fde9
Alex <alex.kliger@gmail.com>
2022-09-27 14:03:42 -0400

    refactored to account for change in backend data structure

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,14 +1,17 @@
-import { startGame, nextQuestion, prevQuestion, submitAnswer } from '../util/api'
+import { startGame, resetGame, nextQuestion, prevQuestion, submitAnswer } from '../util/api'
 
 const GAME_STATE = {
     WAITING_FOR_PLAYERS: 'waiting-for-players',
-    GAME_STARTED: 'game-started'
+    GAME_STARTED: 'game-started',
+    GAME_ENDED: 'game-ended'
 }
 
 const Game = ({ game, handleLeaveGame, setGame, playerId }) => {
     return (
         <section>
-            {game.state === GAME_STATE.GAME_STARTED &&
+            {game.state === GAME_STATE.WAITING_FOR_PLAYERS && <h2>Waiting for players...</h2>}
+            {
+            game.state === GAME_STATE.GAME_STARTED &&
             <div>
                 <h2>Game Started</h2>
                 <p>What is {game.questions[game.currentQuestion].question}?</p>
@@ -22,18 +25,22 @@ const Game = ({ game, handleLeaveGame, setGame, playerId }) => {
                 </ul>
                 <button onClick={async () => setGame(await prevQuestion(game.gameId))}>Prev</button>
                 <button onClick={async () => setGame(await nextQuestion(game.gameId))}>Next</button>
-            </div>}
+            </div>
+            }
 
             <ul>
                 <h3>Players</h3>
                 {game.players.map((player, key) => (
-                    <li key={key}>{player.playerId}</li>
+                <li key={key}>{player.playerId} score: {player.score}</li>
                 ))}
             </ul>
 
-            {game.state === GAME_STATE.WAITING_FOR_PLAYERS &&
-            <button onClick={ async () => setGame(await startGame(game.gameId)) }>Start Game</button>}
+            {
+            game.state === GAME_STATE.WAITING_FOR_PLAYERS &&
+            <button onClick={ async () => setGame(await startGame(game.gameId)) }>Start Game</button>
+            }
             <button onClick={ handleLeaveGame }>Leave Game</button>
+            <button onClick={ async () => setGame(await resetGame(game.gameId)) }>Reset Game</button>
         </section>   
     )
 }
