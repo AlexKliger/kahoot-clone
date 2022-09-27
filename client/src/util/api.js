9e03ae245f7182962d751a19f83a4d987861482c
Alex <alex.kliger@gmail.com>
2022-09-27 13:02:09 -0400

    fixed bug causing resetGame to throw an error

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,18 +1,23 @@
 import { GET, DELETE, POST, PUT } from './fetch'
 
 export const joinGame = async (playerId, gameId) => {
-    const config = {body: JSON.stringify({playerId: playerId, gameId: gameId})}
+    const config = {body: JSON.stringify({ playerId: playerId, gameId: gameId })}
     return await POST('http://localhost:5000/game/join', config)
 }
 
 export const leaveGame = async (playerId, gameId) => {
-    const config = {body: JSON.stringify({playerId: playerId, gameId: gameId})}
+    const config = {body: JSON.stringify({ playerId: playerId, gameId: gameId })}
     return await POST('http://localhost:5000/game/leave', config)
 }
 
 export const startGame = async (gameId) => {
-    const config = {body: JSON.stringify({gameId: gameId})}
+    const config = {body: JSON.stringify({ gameId: gameId })}
     return await POST('http://localhost:5000/game/start', config)
+}
+
+export const resetGame = async (gameId) => {
+    const config = {body: JSON.stringify({ gameId: gameId })}
+    return await POST('http://localhost:5000/game/reset', config)
 }
 
 export const nextQuestion = async (gameId) => {
