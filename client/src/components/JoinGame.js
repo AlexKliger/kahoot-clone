32eff1db56ae2e0dcee57996ac9dbf8dea391696
Alex <alex.kliger@gmail.com>
2022-09-28 16:52:22 -0400

    working build

---
+++ client/src/components/JoinGame.js
@@ -0,0 +1,24 @@
+import { useCallback, useState } from "react"
+
+const JoinGame = ({ handleSubmit }) => {
+    const [name, setName] = useState('')
+
+    const onSubmit = useCallback(e => {
+        handleSubmit(name)
+        e.preventDefault()
+    }, [name])
+
+    return (
+        <form onSubmit={onSubmit}>
+            <input
+                type="text"
+                value={name}
+                onChange={e => setName(e.target.value)}
+            ></input>
+
+            <input type="submit" value="Join Game"></input>
+        </form>
+    )
+}
+
+export default JoinGame
