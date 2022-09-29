8079b03a876be90ea5a8fd3635146f7e7382c694
Alex <alex.kliger@gmail.com>
2022-09-29 15:23:05 -0400

    styling

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,3 +1,4 @@
+import Players from './Players'
 import { startGame, resetGame, nextQuestion, prevQuestion, submitAnswer } from '../util/api'
 
 const GAME_STATE = {
@@ -8,68 +9,53 @@ const GAME_STATE = {
 
 const Game = ({ game, handleLeaveGame, setGame, playerId }) => {
     return (
-        <section>
-            {
-            game.state === GAME_STATE.WAITING_FOR_PLAYERS &&
-            <div>
-                <ul>
-                    <h2>Waiting for players...</h2>
-                    {game.players.map((player, key) => (
-                    <li key={key}>{player.playerName}</li>
-                    ))}
-                </ul>
+        <section className="game flex-centered margin-centered">
+            {game.state === GAME_STATE.WAITING_FOR_PLAYERS &&
+            <section className="game__waiting-room">
+                <h2 className="font-size--extra-large">Waiting for players...</h2>
+                <Players players={game.players} />
+                {game.host === playerId &&
                 <button onClick={ async () => setGame(await startGame(game.gameId)) }>Start Game</button>
-            </div>
+                }       
+            </section>
             }
 
-            {
-            game.state === GAME_STATE.GAME_STARTED &&
-            <div>
-                <h2>Game Started</h2>
-                <p>What is {game.questions[game.currentQuestion].question}?</p>
-                <ul>
-                    <h3>Options</h3>
-                    {
-                    game.questions[game.currentQuestion].choices.map((choice, key) => (
-                        <li key={key}><button onClick={() => submitAnswer(playerId, game.gameId, key)}>{choice}</button></li>
-                    ))
-                    }   
-                </ul>
+            {game.state === GAME_STATE.GAME_STARTED &&
+            <section className="game__screen">
+                <div className="question">
+                    <h3 className="font-size--large">Question: {game.currentQuestion+1}/{game.questions.length}</h3>
+                    <p>What is {game.questions[game.currentQuestion].question}?</p>
+                    <ul className="answer-choices">
+                        {game.questions[game.currentQuestion].choices.map((choice, key) => (
+                            <li key={key}><button onClick={() => submitAnswer(playerId, game.gameId, key)}>{choice}</button></li>
+                        ))}   
+                    </ul>
+                </div>
 
-                {
-                game.host === playerId && 
-                <div>
-                    <button onClick={async () => setGame(await prevQuestion(game.gameId))}>Prev</button>
-                    <button onClick={async () => setGame(await nextQuestion(game.gameId))}>Next</button>
+                {game.host === playerId && 
+                <div className="host-panel">
+                    <button className="host-panel__prev-button" onClick={async () => setGame(await prevQuestion(game.gameId))}>Prev</button>
+                    <button className="host-panel__next-button" onClick={async () => setGame(await nextQuestion(game.gameId))}>Next</button>
+                    <button className="host-panel__reset-button" onClick={ async () => setGame(await resetGame(game.gameId)) }>Reset Game</button>
                 </div>
                 }
 
-
-                <ul>
-                    <h3>Players</h3>
-                    {game.players.map((player, key) => (
-                    <li key={key}>{player.playerName} score: {player.score}</li>
-                    ))}
-                </ul>
-            </div>
+                <Players players={game.players} />
+            </section>
             }
 
             {
             game.state === GAME_STATE.GAME_ENDED &&
-            <ul>
-                <h2>Waiting for players...</h2>
-                {game.players.map((player, key) => (
-                <li key={key}>{player.playerName} score: {player.score}</li>
-            ))}
-            </ul>
+            <section>
+                <h2 className="font-size--extra-large">Game Ended</h2>
+                <Players players={game.players} />
+                {game.host === playerId &&
+                <button className="host-panel__reset-button" onClick={ async () => setGame(await resetGame(game.gameId)) }>Reset Game</button>
+                }
+            </section>
             }
 
             <button onClick={ handleLeaveGame }>Leave Game</button>
-
-            {
-            game.host === playerId &&
-            <button onClick={ async () => setGame(await resetGame(game.gameId)) }>Reset Game</button>
-            }
         </section>   
     )
 }
