fcb01e67d669cc70990c0806311dd6b5c86d182f
Alex <alex.kliger@gmail.com>
2022-10-08 12:55:10 -0400

    working build

---
+++ client/src/components/Checkbox.js
@@ -0,0 +1,15 @@
+const Checkbox = ({handleChange, label}) => {
+
+    return (
+      <label>
+        {label || ""}
+        <input
+          type="checkbox"
+          onChange={handleChange}
+        >
+        </input>
+      </label>
+    )
+  }
+  
+  export default Checkbox
