541922d2eea9c6a8e71b13a584283e315d171a5f
Alex <alex.kliger@gmail.com>
2022-10-16 14:02:31 -0400

    redesigned how the Plus operator adjusts numbers for non-regrouping operations

--- server/questionGenerator/operator.js
+++ server/questionGenerator/operator.js
@@ -74,27 +74,33 @@ class Plus extends Operator {
     }
 
     adjustForNoRegrouping() {
-        console.log('original:', this.leftNum.value, '+', this.rightNum.value)
-        // Convert numbers to string and pad with 0 to make indexing easier.
-        const maxDigits = Math.max(this.leftNum.digits, this.rightNum.digits)
-        const leftNumValue = this.leftNum.value.toString().padStart(maxDigits, 0)
-        const rightNumValue = this.rightNum.value.toString().padStart(maxDigits, 0)
-        // For each place value, starting from the "one's place"...
-        for (let i = maxDigits - 1; i >= 0; i--) {
-            const digitOfLeft = parseInt(leftNumValue[i])
-            const digitOfRight = parseInt(rightNumValue[i])
-            // If the sum of the given place value is greater than 10...
-            if (digitOfLeft + digitOfRight > 9) {
-                console.log('digitOfLeft:', digitOfLeft, 'digitOfRight:', digitOfRight)
-                const amountOverNine = digitOfLeft + digitOfRight - 9
-                // Subtract the amount-over-nine from the given digit from either number.
-                if (Math.round(Math.random() * 1)) {
-                    this.leftNum.value = this.leftNum.value - amountOverNine * 10 ** (maxDigits - 1 - i)
+        console.log('adjustForNoRegrouping')
+        console.log('   leftNum:', this.leftNum.value, 'rightNum:', this.rightNum.value)
+        const numWithMostDigits = this.leftNum.digits > this.rightNum.digits ? this.leftNum : this.rightNum
+        const decimalPlaces = numWithMostDigits.decimalPlaces || 0
+        let valueL = this.leftNum.value
+        let valueR = this.rightNum.value
+        let digitL, digitR;
+        for (let i = numWithMostDigits.digits - decimalPlaces - 1; i >= -decimalPlaces; i--) {
+            digitL = Math.round(valueL / 10**i)
+            valueL -= digitL * 10**i
+            digitR = Math.round(valueR / 10**i)
+            valueR -= digitR * 10**i
+            console.log(`   digitL: ${digitL}  digitR: ${digitR} in ${10**i}'s place`)
+
+            if (digitL + digitR < 10) continue
+            while (digitL + digitR + Math.floor(Math.random() * 10) >= 10) {
+                const coinFlip = Math.round(Math.random())
+                if (coinFlip && digitL > 0) {
+                    this.leftNum.value -= 10**i
+                    digitL--
                 } else {
-                    this.rightNum.value = this.rightNum.value - amountOverNine * 10 ** (maxDigits - 1 - i)
+                    this.rightNum.value -= 10**i
+                    digitR--
                 }
+                console.log('       ', digitL, '+', digitR, '=', digitL + digitR)
             }
-        }
+        }       
     }
 }
 
@@ -187,9 +193,9 @@ class DivideBy extends Operator {
     }
 }
 
-const dec1 = new number.Integer('positive', 2, 1)
-const dec2 = new number.Integer('positive', 2, 1)
-const plus = new Times(dec1, dec2, false)
+const dec1 = new number.Decimal('positive', 1, 1)
+const dec2 = new number.Decimal('positive', 1, 1)
+const plus = new Plus(dec1, dec2, false)
 console.log(plus.generateQuestionString())
 
 module.exports = {Operator: Operator, Plus: Plus, Minus: Minus, Times: Times, DivideBy: DivideBy}
