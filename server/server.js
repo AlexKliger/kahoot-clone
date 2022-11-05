23f46e773366dc861f5f271ecb0fc55a83480ff3
Alex <alex.kliger@gmail.com>
2022-11-05 10:05:07 -0400

    fixed react router refresh bug

--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
 // Route imports
 const gameRoutes = require('./routes/game')
 // Config imports
@@ -16,7 +13,9 @@ const app = express()
 connectDB()
 
 // Express middleware
-app.use(express.static(path.join(__dirname, '../client/build')))
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+app.use(express.static('../client/public'))
+
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(cors())
@@ -24,9 +23,9 @@ app.use(cors())
 // Route middleware
 app.use('/game', gameRoutes)
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
     try {
-        res.sendFile(__dirname + '/../client/build/index.html')
+        res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
     } catch (err) {
         console.log(err)
     }
