ef9dd226a5f35f2110543dc1b4f290ca33260309
Alex <alex.kliger@gmail.com>
2022-11-05 22:13:22 -0400

    selected answer now highlights for given user

--- client/src/components/GameScreen.js
+++ client/src/components/GameScreen.js
@@ -1,24 +1,25 @@
 import { submitAnswer } from '../util/api'
 
 const GameScreen = ({ game, setGame, playerId }) => {
-    return (
-        <section className="game-screen">
-            <span className="question font-size--extra-large">{game.questions[game.currentQuestion].question}</span>
-            <ul className="answer-choices">
-                {game.questions[game.currentQuestion].choices.map((choice, key) => (
-                    <li key={key}>
-                        <button
-                            className="answer-choices__choice font-size--extra-large"
-                            onClick={() => submitAnswer(playerId, game.gameId, key)}
-                        >
-                            {choice}
-                        </button>
-                    </li>
-                ))}   
-            </ul>
+  console.log('GameScreen:', game.submittedAnswers)
+  return (
+    <section className="game-screen">
+      <span className="question font-size--extra-large">{game.questions[game.currentQuestion].question}</span>
+      <ul className="answer-choices">
+        {game.questions[game.currentQuestion].choices.map((choice, key) => (
+          <li key={key}>
+            <button
+              className={`answer-choices__choice font-size--extra-large ${game.submittedAnswers[game.currentQuestion][playerId] === key ? 'answer-choices__choice--selected' : ''}`}
+              onClick={async () => setGame(await submitAnswer(playerId, game.gameId, key))}
+            >
+              {choice}
+            </button>
+          </li>
+        ))}   
+      </ul>
 
-        </section>
-    )
+    </section>
+  )
 }
 
 export default GameScreen
