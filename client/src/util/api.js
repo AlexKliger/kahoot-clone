397fec7705efa5f8bad7e1c576bbd371aa686aa2
Alex <alex.kliger@gmail.com>
2022-11-14 17:17:48 -0500

    fixed submitAnswer sending undefined answer index

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,41 +1,48 @@
 import { GET, DELETE, POST, PUT } from './fetch'
 
 export const createGame = async (hostId, leftNumConfig, rightNumConfig, operatorConfig) => {
-    const config = {body: JSON.stringify({ hostId: hostId, leftNumConfig: leftNumConfig, rightNumConfig: rightNumConfig, operatorConfig: operatorConfig })}
+    const config = {body: JSON.stringify({
+        hostId,
+        leftNumConfig,
+        rightNumConfig,
+        operatorConfig })}
     return await POST('/game/create', config)
 }
 
 export const joinGame = async (playerId, playerName, gameId) => {
-    const config = {body: JSON.stringify({ playerId: playerId, playerName: playerName, gameId: gameId })}
+    const config = {body: JSON.stringify({
+        playerId,
+        playerName,
+        gameId })}
     return await POST('/game/join', config)
 }
 
 export const leaveGame = async (playerId, gameId) => {
-    const config = {body: JSON.stringify({ playerId: playerId, gameId: gameId })}
+    const config = {body: JSON.stringify({ playerId, gameId })}
     return await POST('/game/leave', config)
 }
 
 export const startGame = async (gameId) => {
-    const config = {body: JSON.stringify({ gameId: gameId })}
+    const config = {body: JSON.stringify({ gameId })}
     return await POST('/game/start', config)
 }
 
 export const resetGame = async (gameId) => {
-    const config = {body: JSON.stringify({ gameId: gameId })}
+    const config = {body: JSON.stringify({ gameId })}
     return await POST('/game/reset', config)
 }
 
 export const nextQuestion = async (gameId) => {
-    const config = {body: JSON.stringify({ gameId: gameId })}
+    const config = {body: JSON.stringify({ gameId })}
     return await POST('/game/questions/next', config)
 }
 
 export const prevQuestion = async (gameId) => {
-    const config = {body: JSON.stringify({ gameId: gameId })}
+    const config = {body: JSON.stringify({ gameId })}
     return await POST('/game/questions/previous', config)
 }
 
-export const submitAnswer = async (questionId, playerId, answer) => {
-    const config = {body: JSON.stringify({ playerId: playerId, answerIndex: answer })}
+export const submitAnswer = async (questionId, playerId, answerIndex) => {
+    const config = {body: JSON.stringify({ playerId, answerIndex })}
     return await POST(`/game/questions/submitAnswer/${questionId}`, config)
 }
