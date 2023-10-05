c4fad6fceec9096ced2e9bd4ec0d3728eb0d91db
Alex <alex.kliger@gmail.com>
2023-10-04 21:55:03 -0400

    migrate to use context

--- client/src/components/landing-page/LandingPage.js
+++ client/src/components/landing-page/LandingPage.js
@@ -1,6 +1,16 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { GameDispatchContext } from '../../context/GameContext'
 import JoinGame from './JoinGameForm'
 
-const LandingPage = ({ handleJoinGame }) => {    
+const LandingPage = () => {    
+    const { joinGame } = useContext(GameDispatchContext)
+    const navigate = useNavigate()
+
+    async function handleJoinGame(fields) {
+        await joinGame(fields.name, fields.gameId)
+        navigate('/game/play', { replace: true })
+    }
 
     return (
         <section className="page margin-centered">
