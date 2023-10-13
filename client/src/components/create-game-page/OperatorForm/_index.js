d6482917db96dc686500172f9a8162fca47db966
Alex <alex.kliger@gmail.com>
2023-10-13 15:02:34 -0400

    styling

--- client/src/components/create-game-page/OperatorForm/_index.js
+++ client/src/components/create-game-page/OperatorForm/_index.js
@@ -16,61 +16,38 @@ const OPERATOR = {
 
 const OperatorForm = ({ operator, setOperator, leftNumType, rightNumType }) => {
     return (
-        <div className="config-form config-form--operator">
-
-                {/* <label htmlFor="operator">
-                    Operator
-                </label> */}
-            <OperatorSelector
-                onChange={ (selection) => setOperator({...operator, type: selection}) }
-                value={operator.type}
-            />
-                {/* <select
-                    className="font-size--medium"
+        <div className="max-w-fit mx-auto">
+            <div className="max-w-fit mx-auto">
+                <OperatorSelector
+                    onChange={ (selection) => setOperator({...operator, type: selection}) }
                     value={operator.type}
-                    name="operator"
-                    id="operator"
-                    onChange={e => setOperator({...operator, type: e.target.value})}>
-                    {Object.values(OPERATOR).map((type, key) => (
-                        <option value={type} key={key}>{type}</option>
-                    ))}
-                </select> */}
+                />
+            </div>
 
-            <div className="config-form__options">
+            <div className="max-w-fit mx-auto">
                 {(operator.type === OPERATOR.PLUS ||
                 operator.type === OPERATOR.MINUS ||
                 operator.type === OPERATOR.TIMES) &&
+                (leftNumType == NUMBER.INTEGER || 
+                leftNumType == NUMBER.DECIMAL) &&
                 <Checkbox
-                    label="Regrouping"
+                    label="Regrouping?"
                     handleChange={e => setOperator({...operator, regrouping: e.target.value})}
                 />}
 
                 {operator.type === OPERATOR.DIVIDE_BY &&
+                leftNumType == NUMBER.INTEGER &&
                 <Checkbox
-                    label="Has remainder"
+                    label="Has remainder?"
                     handleChange={e => setOperator({...operator, hasRemainder: e.target.value})}
                 />}
 
                 {leftNumType === NUMBER.FRACTION && rightNumType === NUMBER.FRACTION &&
                 <Checkbox
-                    label="Common denominators"
+                    label="Common denominators?"
                     handleChange={e => setOperator({...operator, commonDenominators: e.target.value})}
                 />
                 }
-
-                <label>
-                    Answer format
-                    <select
-                        style={{"marginLeft": "0.5rem"}}
-                        className="font-size--medium"
-                        onChange={e => setOperator({...operator, answerFormat: e.target.value})}
-                        value={operator.answerFormat}
-                    >
-                        <option>decimal</option>
-                        <option>ratio</option>
-                    </select>
-                </label>
-
             </div>
 
         </div>
