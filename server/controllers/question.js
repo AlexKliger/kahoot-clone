5ab24434e5a8ff0b020eb6c39de8fca53ac50f97
Alex <alex.kliger@gmail.com>
2023-10-14 16:22:45 -0400

    fixes prev endpoint bug

--- server/controllers/question.js
+++ server/controllers/question.js
@@ -23,7 +23,7 @@ async function previous(req, res) {
         game && await game.prevQuestion()
         io = sockets.getSocketServer()
         io.to(req.body.gameId).emit('data', JSON.stringify(game))
-        res.json.status(200)(game)
+        res.status(200).json(game)
     } catch (err) {
         console.log(err)
     }
